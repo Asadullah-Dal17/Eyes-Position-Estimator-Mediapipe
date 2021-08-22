81be0686e44c89b4ac147eda3e8af3123f7e9a47
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-08-22 16:03:28 +0500

    complete landmarkd  detection

--- Eye_Tracking_part1/main.py
+++ Eye_Tracking_part1/main.py
@@ -1,11 +1,13 @@
-import cv2 as cv 
-import time 
-import utils
+import cv2 as cv
 import mediapipe as mp
-# varables 
-frame_counter =0
-font = cv.FONT_HERSHEY_COMPLEX
+import time
+import utils
 
+# variables 
+frame_counter =0
+
+# constants 
+FONTS =cv.FONT_HERSHEY_COMPLEX
 # Landmarks indices for different parts of face, mediapipe.
 
 # face bounder indices 
@@ -23,59 +25,58 @@ LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
 RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
 RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
 
-# face mesh object 
-map_face_mesh = mp.solutions.face_mesh
-# functions 
-
-# landmarks detector function 
-def landmarksDetector(img, results, draw=False)->list:
-    # getting image height and width 
-    img_height, img_width =img.shape[:2]
-    #looping through normalized landmarks, converting the to img coordinates
-    mesh_marks_coord = [(int(point.x*img_width), int(point.y*img_height)) for point in results.multi_face_landmarks[0].landmark]
-    if draw:
-        # draw circle on each landmark 
-        [cv.circle(img, mark_point, 2, utils.GREEN, -2) for mark_point in mesh_marks_coord]
-    return mesh_marks_coord
-
 # camera object 
 camera = cv.VideoCapture('VideoFile.mp4')
+
+# function
+# landmarks detector fucntion 
+def landmarkDetection(img, results, draw=False):
+    # getting image width and height
+    img_height, img_width = img.shape[:2]
+    # looping through landmarks, converting normalized landmarks to pixel coordinates list[tuples(x,y)]
+    mesh_coords = [(int(point.x*img_width), int(point.y*img_height)) for point in results.multi_face_landmarks[0].landmark]
+    if draw:
+        # draw circle on each landmarks 
+        [cv.circle(frame, point, 2, utils.GREEN, -1) for point in mesh_coords]
+    return mesh_coords # list of tuples(x,y)
+# setting up mediapipe 
+map_face_mesh = mp.solutions.face_mesh
+
+# config mediapipe 
 with map_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
-    # strating time here 
-    starting_time = time.time()
-    # loop through frames 
+
+    # starting time here 
+    start_time = time.time()
+
     while True:
-        frame_counter+=1
-        ret, frame = camera.read()
-        if not ret:
-            break 
-        # converting BGR frame to RGB
+        frame_counter +=1 # frame counter
+        ret, frame = camera.read() # getting frame from camera 
+        if not ret: 
+            break # no more frames break
+        # converting color space of frame 
         rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        
-        # getting the landmarks
+
         results = face_mesh.process(rgb_frame)
-        # checking if landmarks are detected or not first
+        # checking for landmarks 
         if results.multi_face_landmarks:
-            # print(f'landmarks_detected_{frame_counter}')
-            # calling landmark detection function
-            mesh_coord = landmarksDetector(frame, results=results, draw=False)
-            # draw transparent shape on each part, like eyes, lips, face_oval, and eyebrows 
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in FACE_OVAL], utils.GRAY, 0.4)
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in LIPS], utils.PINK, 0.2)
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in LEFT_EYE], utils.BLUE, 0.2)
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in LEFT_EYEBROW], utils.GREEN, 0.2)
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in RIGHT_EYE], utils.BLUE, 0.2)
-            frame = utils.fillPolyTrans(frame, [mesh_coord[point] for point in RIGHT_EYEBROW], utils.GREEN, 0.2)
+            mesh_coord = landmarkDetection(frame, results, False)
+
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in FACE_OVAL], utils.GRAY, 0.6)
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in LEFT_EYE], utils.GREEN, 0.3)
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in LEFT_EYEBROW], utils.YELLOW, 0.3)
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in RIGHT_EYE], utils.GREEN, 0.3)
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in RIGHT_EYEBROW], utils.YELLOW, 0.3)
+            frame = utils.fillPolyTrans(frame, [mesh_coord[p] for p in LIPS], utils.PINK, 0.3)
 
 
-
-
-        end_time = time.time()-starting_time
+        # calc fps 
+        end_time = time.time()-start_time
         fps = frame_counter/end_time
-        cv.putText(frame, f'FPS: {round(fps,2)}', (20,50), font, 1.0, utils.GREEN, 2)
+        cv.putText(frame, f'FPS: {round(fps,2)}', (20, 50), FONTS, 1.0, utils.GREEN, 2 )
+
         cv.imshow('frame', frame)
         key = cv.waitKey(1)
-        if key ==ord('q') or key==ord('Q'):
+        if key==ord('q') or key ==ord('Q'):
             break
     cv.destroyAllWindows()
     camera.release()
