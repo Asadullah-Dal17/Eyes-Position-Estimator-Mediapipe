40d9564d0350079dabb3ac7e1bb7aa05cb4cf802
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-09-05 23:30:22 +0500

    color background text

--- Eye_Tracking_part2/utils.py
+++ Eye_Tracking_part2/utils.py
@@ -33,6 +33,29 @@ def drawColor(img, colors):
         cv.rectangle(img, (x-6, y-5 ), (x+w+5, y+h+5), (10, 50, 10), -1)
         cv.rectangle(img, (x, y ), (x+w, y+h), color, -1)
     
+def colorBackgroundText(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3):
+    """
+    Draws text with background, with  control transparency
+    @param img:(mat) which you want to draw text
+    @param text: (string) text you want draw
+    @param font: fonts face, like FONT_HERSHEY_COMPLEX, FONT_HERSHEY_PLAIN etc.
+    @param fontScale: (double) the size of text, how big it should be.
+    @param textPos: tuple(x,y) position where you want to draw text
+    @param textThickness:(int) fonts weight, how bold it should be
+    @param textPos: tuple(x,y) position where you want to draw text
+    @param textThickness:(int) fonts weight, how bold it should be.
+    @param textColor: tuple(BGR), values -->0 to 255 each
+    @param bgColor: tuple(BGR), values -->0 to 255 each
+    @param pad_x: int(pixels)  padding of in x direction
+    @param pad_y: int(pixels) 1 to 1.0 (), controls transparency of  text background 
+    @return: img(mat) with draw with background
+    """
+    (t_w, t_h), _= cv.getTextSize(text, font, fontScale, textThickness) # getting the text size
+    x, y = textPos
+    cv.rectangle(img, (x-pad_x, y+ pad_y), (x+t_w+pad_x, y-t_h-pad_y), bgColor,-1) # draw rectangle 
+    cv.putText(img,text, textPos,font, fontScale, textColor,textThickness ) # draw in text
+
+    return img
 
 def textWithBackground(img, text, font, fontScale, textPos, textThickness=1,textColor=(0,255,0), bgColor=(0,0,0), pad_x=3, pad_y=3, bgOpacity=0.5):
     """
